a032957b0feeed119c113c21bb28a61bc5ddfdb6
raiffsf <39918331+raiffsf@users.noreply.github.com>
2018-06-08 13:38:05 -0300

    teste_naomtobom
    This reverts commit c12322ad6a6786a33b6cd7af06ee50c152c44037.

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rvmdk/driverlib.lib" -llibc.a
-
