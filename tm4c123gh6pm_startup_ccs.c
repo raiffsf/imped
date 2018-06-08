4fe86bd058ebbce12fcafdaccdb19bb3814accef
raiffsf <39918331+raiffsf@users.noreply.github.com>
2018-06-08 13:25:43 -0300

    Revert "continuous_DAC1"
    This reverts commit 881f131a73b3d4d37c7ea5b74bcbc568332ce3cc.

--- tm4c123gh6pm_startup_ccs.c
+++ tm4c123gh6pm_startup_ccs.c
@@ -102,11 +102,11 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // ADC Sequence 0
     IntDefaultHandler,                      // ADC Sequence 1
     IntDefaultHandler,                      // ADC Sequence 2
-    IntDefaultHandler,                         // ADC Sequence 3
+    ADC0IntHandler,                         // ADC Sequence 3
     IntDefaultHandler,                      // Watchdog timer
     IntDefaultHandler,                      // Timer 0 subtimer A
     IntDefaultHandler,                      // Timer 0 subtimer B
-    IntDefaultHandler,                      // Timer 1 subtimer A
+    Timer1IntHandler,                      // Timer 1 subtimer A
     IntDefaultHandler,                      // Timer 1 subtimer B
     IntDefaultHandler,                      // Timer 2 subtimer A
     IntDefaultHandler,                      // Timer 2 subtimer B
